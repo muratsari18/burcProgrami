46ad15dbcaaba312c3cb178bd5a3dd952cb2876a
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-08 17:42:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,135 @@
 import java.util.Scanner;
 
-public class Main {
-
+public class BurcProgrami {
     public static void main(String[] args) {
-        int n1, n2, select;
+        int month, day;
+        String burc = " ";
+        boolean isError = false;
+
         Scanner input = new Scanner(System.in);
-        System.out.print("Ilk Sayıyı Giriniz:");
-        n1 = input.nextInt();
-        System.out.print("Ikinci Sayıyı Giriniz:");
-        n2 = input.nextInt();
+        System.out.print("Dogdunuz ay : ");
+        month = input.nextInt();
 
-        System.out.println("1-Toplama\n2-Çıkarma\n3-Çarpma\n4-Bölme");
-        System.out.print("Seçiminiz nedir?");
-        select = input.nextInt();
+        System.out.print("Dogdunuz Gun : ");
+        day = input.nextInt();
 
-        switch (select) {
+        switch (month) {
             case 1:
-                System.out.println(n1 + n2);
+                if ((day >= 1) && (day <= 31))
+                    if (day < 22)
+                        burc = "Oglak Burcu";
+                    else
+                        burc = "Kova Burcu";
+                else
+                    isError = true;
                 break;
             case 2:
-                System.out.println(n1 - n2);
+                if ((day >= 1) && (day <= 29))
+                    if (day <= 19)
+                        burc = "Kova Burcu";
+                    else
+                        burc = "Balik Burcu";
+                else
+                    isError = true;
                 break;
             case 3:
-                System.out.println(n1 * n2);
+                if ((day >= 1) && (day <= 31))
+                    if (day <= 20)
+                        burc = "Balik Burcu";
+                    else
+                        burc = "Koc Burcu";
+                else
+                    isError = true;
                 break;
             case 4:
-                if(n2!=0)
-                System.out.println(n1 / n2);
+                if ((day >= 1) && (day <= 30))
+                    if (day <= 20)
+                        burc = "Koc Burcu";
+                    else
+                        burc = "Boga Burcu";
                 else
-                    System.out.println("Bir sayı 0'a bolunemez!");
+                    isError = true;
+                break;
+            case 5:
+                if ((day >= 1) && (day <= 31))
+                    if (day <= 20)
+                        burc = "Boga Burcu";
+                    else
+                        burc = "İkizler Burcu";
+                else
+                    isError = true;
+                break;
+            case 6:
+                if ((day >= 1) && (day <= 30))
+                    if (day <= 22)
+                        burc = "İkizler Burcu";
+                    else
+                        burc = "Yengec Burcu";
+                else
+                    isError = true;
+                break;
+            case 7:
+                if ((day >= 1) && (day <= 31))
+                    if (day <= 22)
+                        burc = "Yengec Burcu";
+                    else
+                        burc = "Aslan Burcu";
+                else
+                    isError = true;
+                break;
+            case 8:
+                if ((day >= 1) && (day <= 30))
+                    if (day <= 22)
+                        burc = "Aslan Burcu";
+                    else
+                        burc = "Başak Burcu";
+                else
+                    isError = true;
+                break;
+            case 9:
+                if ((day >= 1) && (day <= 31))
+                    if (day <= 22)
+                        burc = "Basak Burcu";
+                    else
+                        burc = "Terazi Burcu";
+                else
+                    isError = true;
+                break;
+            case 10:
+                if ((day >= 1) && (day <= 30))
+                    if (day <= 22)
+                        burc = "Terazi Burcu";
+                    else
+                        burc = " Akrep Burcu";
+                else
+                    isError = true;
+                break;
+            case 11:
+                if ((day >= 1) && (day <= 31))
+                    if (day <= 21)
+                        burc = "Akrep Burcu";
+                    else
+                        burc = "Yay Burcu";
+                else
+                    isError = true;
+                break;
+            case 12:
+                if ((day >= 1) && (day <= 30))
+                    if (day <= 21)
+                        burc = "Yay Burcu";
+                    else
+                        burc = "Oglak Burcu";
+                else
+                    isError = true;
                 break;
             default:
-                System.out.println("Araligin disinda bir deger sectiniz");
-
+                isError = true;
 
         }
+        if (isError)
+            System.out.println("Yanlis bir tarih girdiniz");
+        else
+            System.out.println("Merhaba sizin burcunuz : " + burc);
 
     }
 }
